22aa3062a49e9053b8cc64e4072b0d45cdab3fac
Soham Patil <edu.sohampatil@gmail.com>
2026-02-02 14:01:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6052189616-97792",
+  "appId": "1:1066102867021:web:08be99056969ca9c4c4804",
+  "apiKey": "AIzaSyBiqsp075Zal-LxGEZs-vLtHYxpWWeRWGE",
+  "authDomain": "studio-6052189616-97792.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1066102867021"
+};
